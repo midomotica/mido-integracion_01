382b3a36b732194b38e33ab917279baed081eb9c
midomotica <jbrard@midomotica.cl>
2025-07-17 19:21:39 -0400

    Update __init__.py

--- custom_components/ejemplo_sensor/__init__.py
+++ custom_components/ejemplo_sensor/__init__.py
@@ -1,4 +1,4 @@
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.config_entries import ConfigEntry, async_forward_entry_setup, async_forward_entry_unload
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
@@ -6,11 +6,10 @@ async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "binary_sensor")
-    )
+    await async_forward_entry_setup(hass, entry, "binary_sensor")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "binary_sensor")
+    return await async_forward_entry_unload(hass, entry, "binary_sensor")
+
 
