8338d16f10b4b708a4a1459f798bf39b393f5bdc
midomotica <jbrard@midomotica.cl>
2025-07-17 16:47:02 -0400

    Update config_flow.py

--- custom_components/ejemplo_sensor/config_flow.py
+++ custom_components/ejemplo_sensor/config_flow.py
@@ -1,1 +1,18 @@
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+import voluptuous as vol
+from .const import DOMAIN, DEFAULT_NAME
 
+class EjemploConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
+            })
+        )
