c054c77050e5d68feb5412f460822b2c39ebfc09
midomotica <jbrard@midomotica.cl>
2025-07-17 18:55:30 -0400

    Update __init__.py

--- custom_components/ejemplo_sensor/__init__.py
+++ custom_components/ejemplo_sensor/__init__.py
@@ -7,7 +7,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
+        hass.config_entries.async_forward_entry_setups(entry, "binary_sensor")
     )
     return True
 
